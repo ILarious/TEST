e92eee8612123b0777bcfa529e0c732540a2ad7e
Ilya Badaev <ilarious08@yandex.ru>
2023-09-09 19:27:17 +0300

    add tree

--- frontend/js/task.js
+++ frontend/js/task.js
@@ -1,42 +1,14 @@
 const taskRequestURL = 'http://127.0.0.1:8000/tasks/'
-
+const treeContainer = document.getElementById('tree-container');
 
 function getTasks() {
     sendRequest('GET', taskRequestURL)
         .then(data => {
             console.log(data)
-            buildTree(data)
+            buildTree(data, treeContainer, null)
         })
         .catch(err => console.log(err))
 }
-
-
-function buildTree(data) {
-    const treeRoot = document.getElementById('tree-root')
-
-    function buildLevel(parentId, parentElement) {
-        const childTasks = data.filter(task => task.parent_id === parentId);
-
-        if (childTasks.length === 0) {
-            return;
-        }
-
-        const ul = document.createElement('ul');
-        parentElement.appendChild(ul);
-
-        childTasks.forEach(task => {
-            const li = document.createElement('li');
-            li.textContent = task.title;
-            ul.appendChild(li);
-
-            // Рекурсивно вызываем функцию для построения подзадач
-            buildLevel(task.id, li);
-        });
-    }
-
-    buildLevel(null, treeRoot)
-}
-
 
 function createTask() {
     let TaskTitle = document.querySelector('#taskTitle').value
@@ -63,4 +35,40 @@ function createTask() {
     document.querySelector('#taskDeadline').value = ''
     document.querySelector('#taskStatus').value = ''
 
+}
+
+
+function buildTree(data) {
+    const treeRoot = document.getElementById('tree-root'); // Это корневой элемент дерева в вашем HTML
+
+    // Рекурсивная функция для построения уровней дерева
+    function buildLevel(parentId, parentElement) {
+        const childTasks = data.filter(task => task.parent_id === parentId);
+
+        if (childTasks.length === 0) {
+            return;
+        }
+
+        const ul = document.createElement('ul');
+        parentElement.appendChild(ul);
+
+        childTasks.forEach(task => {
+            const li = document.createElement('li');
+            const div = document.createElement('div');
+            const h3 = document.createElement('h3');
+
+            div.classList.add('task__content');
+
+            h3.textContent = task.title;
+            ul.appendChild(li);
+            li.appendChild(div)
+            div.appendChild(h3);
+
+            // Рекурсивно вызываем функцию для построения подзадач
+            buildLevel(task.id, li);
+        });
+    }
+
+    // Начинаем с корневого уровня (если у вас есть корневая задача)
+    buildLevel(null, treeRoot);
 }
